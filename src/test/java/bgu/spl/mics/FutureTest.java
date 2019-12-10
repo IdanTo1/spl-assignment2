a399f34e72a59ab8794b999fab7c79421b63dd1d
Idan Tomer <idan.tomer2@gmail.com>
2019-12-10 16:23:16 +0200

    implement Future and Inventory tests
    Signed-off-by: Idan Tomer <idan.tomer2@gmail.com>

--- src/test/java/bgu/spl/mics/FutureTest.java
+++ src/test/java/bgu/spl/mics/FutureTest.java
@@ -2,17 +2,30 @@ package bgu.spl.mics;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.concurrent.TimeUnit;
 
 public class FutureTest {
-    @BeforeEach
-    public void setUp(){
+    Future<String> f;
+    TimeUnit t;
+    public static final String TEST_STRING = "I'm working";
 
+    @BeforeEach
+    public void setUp() {
+        f = new Future<String>();
+        t = TimeUnit.MILLISECONDS;
     }
 
     @Test
-    public void test(){
-        //TODO: change this test and add more tests :)
-        fail("Not a good test");
+    public void test() {
+        assertTrue(f.get(100, t) == null, "unresolved future differs from null");
+        assertTrue(f.isDone() == false, "unresolved future marked as done");
+        f.resolve(TEST_STRING);
+        assertTrue(f.get(100, t) == TEST_STRING, "unresolved future differs from null");
+        assertTrue(f.get() == TEST_STRING, "unresolved future differs from null");
+        assertTrue(f.isDone() == true, "resolved future marked as undone");
     }
 }
--- src/test/java/bgu/spl/mics/InventoryTest.java
+++ src/test/java/bgu/spl/mics/InventoryTest.java
@@ -1,19 +1,50 @@
 package bgu.spl.mics;
 
+import bgu.spl.mics.application.passiveObjects.Inventory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Vector;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class InventoryTest {
+    public static final String[] TEST_GADGETS= {"gadget1", "gadget2", "gadget3", "gadget4"};
+    public static final String TEST_FILE= "./testInventory.json";
+    Inventory inv;
     @BeforeEach
     public void setUp(){
-
+        inv = Inventory.getInstance();
     }
 
     @Test
-    public void test(){
-        //TODO: change this test and add more tests :)
-        fail("Not a good test");
+    public void testSingleTon(){
+        assertTrue(inv != null, "singleTon not created");
+        Inventory otherInstance = Inventory.getInstance();
+        assertTrue(inv == otherInstance, "different instances of singleton Inventory exist!");
+    }
+
+    @Test
+    public void testLoad(){
+        inv.load(TEST_GADGETS);
+        for (String gadget: TEST_GADGETS) {
+            assertTrue(inv.getItem(gadget) == true, "item is not in the inventory");
+            assertTrue(inv.getItem(gadget) == false, "item is in the inventory after it was acquired");
+        }
+    }
+
+    @Test
+    public void testPrintToFile(){
+        inv.load(TEST_GADGETS);
+        inv.printToFile(TEST_FILE);
+        for (String gadget: TEST_GADGETS) {
+            inv.getItem(gadget); // flush inventory
+        }
+        String[] tmp = {};
+        inv.load(tmp); //TODO complete read to string array json
+        for (String gadget: TEST_GADGETS) {
+            assertTrue(inv.getItem(gadget) == true, "item is not in the inventory");
+        }
     }
 }
