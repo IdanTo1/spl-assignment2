e99ac5c8e70aabd1f1cce86ac645621af6e9de38
Roy Hershfinkel <hersh.roy@gmail.com>
2019-12-10 12:38:54 +0200

    Add MessageBroker tests

--- src/test/java/bgu/spl/mics/MessageBrokerTest.java
+++ src/test/java/bgu/spl/mics/MessageBrokerTest.java
@@ -1,5 +1,6 @@
 package bgu.spl.mics;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,15 +8,98 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class MessageBrokerTest {
+    TestSubscriber s1;
+    TestSubscriber s2;
+    MessageBroker m;
     @BeforeEach
     public void setUp(){
+        m = MessageBrokerImpl.getInstance();
+        s1 = new TestSubscriber("s1");
+        s2 = new TestSubscriber("s2");
 
+    }
+
+    @AfterEach
+    public void tearDown() {
+        m.unregister(s1);
+        m.unregister(s2);
     }
 
     @Test
     public void testSingleton(){
-        MessageBroker instance1 = MessageBrokerImpl.getInstance();
-        MessageBroker instance2 = MessageBrokerImpl.getInstance();
-        assertTrue(instance1==instance2, "different instances of singleton MessageBroker exist!");
+        MessageBroker otherInstance = MessageBrokerImpl.getInstance();
+        assertTrue(otherInstance==m, "different instances of singleton MessageBroker exist!");
     }
+
+    @Test
+    public void testEventSubscribe() {
+        m.sendEvent(new IntEvent(0));
+        m.sendEvent(new IntEvent(1));
+        safeAwaitMessage(true, true);
+        assertTrue(s1.getNum() == 0, "Subscriber 1 didn't get the first called event");
+        assertTrue(s2.getNum() == 1, "Subscriber 2 didn't get the second called event");
+        safeAwaitMessage(true, false);
+        assertTrue(s1.getNum() == 2, "Subscriber 1 didn't appear 3rd in a 2 object round-robin");
+    }
+
+    @Test
+    public void testBroadcastSubscribe() {
+        m.sendBroadcast(new IntBroadcast(2));
+        safeAwaitMessage(true, true);
+        assertTrue(s1.getNum() == 2, "Subscriber s1 didn't get the broadcast");
+        assertTrue(s2.getNum() == 2, "Subscriber s2 didn't get the broadcast");
+        m.sendBroadcast(new IntBroadcast(3));
+        safeAwaitMessage(true, true);
+        assertTrue(s1.getNum() == 3, "Subscriber s1 didn't get the broadcast");
+        assertTrue(s2.getNum() == 3, "Subscriber s2 didn't get the broadcast");
+    }
+
+    @Test
+    public void testComplete() {
+        Event<Integer> e = new IntEvent(1);
+        Future<Integer> f = m.sendEvent(e);
+        assertTrue(!f.isDone(), "Future object starts completed");
+        m.complete(e, 2);
+        assertTrue(f.isDone(), "Future object doesn't complete");
+    }
+
+    @Test
+    public void testUnregister() {
+        m.unregister(s2);
+        Future<Integer> f = m.sendEvent(new IntEvent(1));
+        assertTrue(f!=null, "sendEvent should return future object");
+        m.unregister(s1);
+        f = m.sendEvent(new IntEvent(1));
+        assertTrue(f==null,
+                "sendEvent should return null future event when" +
+                        " no subscriber is subscribed to messages of certain type");
+    }
+
+    @Test
+    public void testInterruption() {
+        Thread th = new Thread(() -> {
+            try {
+                m.awaitMessage(s1);
+            }
+            catch (InterruptedException e) {
+                return;
+            }
+            fail("didn't get interrupted while in infinite wait");
+        });
+        th.start();
+        th.interrupt();
+    }
+
+    private void safeAwaitMessage(boolean awaitS1, boolean awaitS2) {
+        try {
+            if(awaitS1) m.awaitMessage(s1);
+            if(awaitS2) m.awaitMessage(s2);
+        }
+        catch (InterruptedException e)
+        {
+            fail("Got interrupted with only 1 thread");
+        }
+    }
+
+
 }
