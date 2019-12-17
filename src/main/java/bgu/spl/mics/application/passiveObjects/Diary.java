f24122cd04d63f1a588826c8bb92ffc8db4c97ba
Roy Hershfinkel <hersh.roy@gmail.com>
2019-12-17 14:32:28 +0200

    Add Diary Implementation

--- src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
@@ -1,6 +1,19 @@
 package bgu.spl.mics.application.passiveObjects;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import com.google.gson.Gson;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Passive object representing the diary where all reports are stored.
@@ -11,16 +24,26 @@ import java.util.List;
  * You can add ONLY private fields and methods to this class as you see fit.
  */
 public class Diary {
+	private static Diary _instance = new Diary();
+	private List<Report> _diary;
+	private ReadWriteLock _lock;
+	private AtomicInteger _total;
+
 	/**
 	 * Retrieves the single instance of this class.
 	 */
 	public static Diary getInstance() {
-		//TODO: Implement this
-		return null;
+		return _instance;
+	}
+
+	private Diary() {
+		_diary = new ArrayList<>();
+		_lock = new ReentrantReadWriteLock();
+		_total = new AtomicInteger(0);
 	}
 
 	public List<Report> getReports() {
-		return null;
+		return _diary;
 	}
 
 	/**
@@ -28,7 +51,9 @@ public class Diary {
 	 * @param reportToAdd - the report to add
 	 */
 	public void addReport(Report reportToAdd){
-		//TODO: Implement this
+		_lock.writeLock().lock();
+		_diary.add(reportToAdd);
+		_lock.writeLock().unlock();
 	}
 
 	/**
@@ -38,8 +63,14 @@ public class Diary {
 	 * List of all the reports in the diary.
 	 * This method is called by the main method in order to generate the output.
 	 */
-	public void printToFile(String filename){
-		//TODO: Implement this
+	public void printToFile(String filename) throws IOException {
+		_lock.readLock().lock();
+		Gson gson = new Gson();
+		String reportsToPrint = gson.toJson(_diary);
+		PrintWriter writer = null;
+		Path file = Paths.get(filename);
+		Files.write(file, Collections.singleton(reportsToPrint), StandardCharsets.UTF_8);
+		_lock.readLock().unlock();
 	}
 
 	/**
@@ -47,14 +78,18 @@ public class Diary {
 	 * @return the total number of received missions (executed / aborted) be all the M-instances.
 	 */
 	public int getTotal(){
-		//TODO: Implement this
-		return 0;
+		return _total.get();
 	}
 
 	/**
 	 * Increments the total number of received missions by 1
 	 */
 	public void incrementTotal(){
-
+		int localTotal;
+		do
+		{
+			localTotal = _total.get();
+		}
+		while (!_total.compareAndSet(localTotal, localTotal+1));
 	}
 }
