c9de80a2730685ccc0f2fb9d57595c3ce918bb6d
Idan Tomer <idan.tomer2@gmail.com>
2019-12-17 13:26:26 +0200

    revert Q to supplied file
    Signed-off-by: Idan Tomer <idan.tomer2@gmail.com>

--- src/main/java/bgu/spl/mics/application/subscribers/Q.java
+++ src/main/java/bgu/spl/mics/application/subscribers/Q.java
@@ -1,41 +1,24 @@
 package bgu.spl.mics.application.subscribers;
 
-import bgu.spl.mics.Future;
 import bgu.spl.mics.Subscriber;
-import bgu.spl.mics.application.messages.GadgetAvailableEvent;
-import bgu.spl.mics.application.messages.GadgetAvailableObject;
-import bgu.spl.mics.application.messages.TickBroadcast;
-import bgu.spl.mics.application.passiveObjects.Inventory;
 
 /**
  * Q is the only Subscriber\Publisher that has access to the {@link bgu.spl.mics.application.passiveObjects.Inventory}.
- * <p>
- * You can add private fields and public methods to this class. You MAY change constructor signatures and even add new
- * public constructors.
+ *
+ * You can add private fields and public methods to this class.
+ * You MAY change constructor signatures and even add new public constructors.
  */
 public class Q extends Subscriber {
 
-    private int _currTime = 0;
-    private Inventory inventory;
+	public Q() {
+		super("Change_This_Name");
+		// TODO Implement this
+	}
 
-    public Q() {
-        super("Q");
-        inventory = Inventory.getInstance();
-    }
-
-    @Override
-    protected void initialize() {
-		subscribeBroadcast(TickBroadcast.class, (TickBroadcast b)->_currTime = b.getCurrTime());
-        subscribeEvent(GadgetAvailableEvent.class, (GadgetAvailableEvent e)->
-        {
-            GadgetAvailableObject result = e.getObj();
-            String gadget = result.getGadget();
-            if (inventory.getItem(gadget)) {
-                result.setGadgetExists();
-            }
-            result.setQtime(_currTime);
-            complete(e, result);
-        });
-    }
+	@Override
+	protected void initialize() {
+		// TODO Implement this
+		
+	}
 
 }
