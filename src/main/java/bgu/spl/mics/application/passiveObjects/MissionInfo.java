6cfaa9ecf31e6b801e87e91782d42d14fcd94dd2
Idan Tomer <idan.tomer2@gmail.com>
2019-12-18 15:27:27 +0200

    adapt naming convention to input.json file
    Signed-off-by: Idan Tomer <idan.tomer2@gmail.com>

--- src/main/java/bgu/spl/mics/application/passiveObjects/MissionInfo.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/MissionInfo.java
@@ -1,6 +1,5 @@
 package bgu.spl.mics.application.passiveObjects;
 
-import javax.print.DocFlavor;
 import java.util.List;
 
 /**
@@ -10,91 +9,95 @@ import java.util.List;
  * You may add ONLY private fields and methods to this class.
  */
 public class MissionInfo {
-	private String _missionName, _gadget;
-	private List<String>  _serialAgentsNumber;
-	private int _timeIssued, _timeExpired, _duration;
+	// names in this class won't stand with naming conventions of the project, to fit input.json File convention
+	private List<String> serialAgentsNumbers;
+	private int duration;
+	private String gadget;
+	private String missionName;
+	private int timeExpired;
+	private int timeIssued;
 
     /**
      * Sets the name of the mission.
      */
     public void setMissionName(String missionName) {
-        _missionName = missionName;
+        this.missionName = missionName;
     }
 
 	/**
      * Retrieves the name of the mission.
      */
 	public String getMissionName() {
-		return _missionName;
+		return missionName;
 	}
 
     /**
      * Sets the serial agent number.
      */
     public void setSerialAgentsNumbers(List<String> serialAgentsNumbers) {
-        _serialAgentsNumber = serialAgentsNumbers;
+        this.serialAgentsNumbers = serialAgentsNumbers;
     }
 
 	/**
      * Retrieves the serial agent number.
      */
 	public List<String> getSerialAgentsNumbers() {
-		return _serialAgentsNumber;
+		return serialAgentsNumbers;
 	}
 
     /**
      * Sets the gadget name.
      */
     public void setGadget(String gadget) {
-        _gadget = gadget;
+        this.gadget = gadget;
     }
 
 	/**
      * Retrieves the gadget name.
      */
 	public String getGadget() {
-		return _gadget;
+		return gadget;
 	}
 
     /**
      * Sets the time the mission was issued in milliseconds.
      */
     public void setTimeIssued(int timeIssued) {
-        _timeIssued = timeIssued;
+        this.timeIssued = timeIssued;
     }
 
 	/**
      * Retrieves the time the mission was issued in milliseconds.
      */
 	public int getTimeIssued() {
-		return _timeIssued;
+		return timeIssued;
 	}
 
     /**
      * Sets the time that if it that time passed the mission should be aborted.
      */
     public void setTimeExpired(int timeExpired) {
-        _timeExpired = timeExpired;
+        this.timeExpired = timeExpired;
     }
 
 	/**
      * Retrieves the time that if it that time passed the mission should be aborted.
      */
 	public int getTimeExpired() {
-		return _timeExpired;
+		return timeExpired;
 	}
 
     /**
      * Sets the duration of the mission in time-ticks.
      */
     public void setDuration(int duration) {
-        _duration = duration;
+        this.duration = duration;
     }
 
 	/**
 	 * Retrieves the duration of the mission in time-ticks.
 	 */
 	public int getDuration() {
-		return _duration;
+		return duration;
 	}
 }
