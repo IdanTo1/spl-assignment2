f1990408d0653d73e52bf89638c53e5d662912cb
Hroy02 <hersh.roy@gmail.com>
2019-12-18 20:17:59 +0200

    Implement TimeService

--- src/main/java/bgu/spl/mics/application/publishers/TimeService.java
+++ src/main/java/bgu/spl/mics/application/publishers/TimeService.java
@@ -1,32 +1,48 @@
 package bgu.spl.mics.application.publishers;
 
+import bgu.spl.mics.Broadcast;
 import bgu.spl.mics.Publisher;
+import bgu.spl.mics.application.messages.TickBroadcast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * TimeService is the global system timer There is only one instance of this Publisher.
  * It keeps track of the amount of ticks passed since initialization and notifies
- * all other subscribers about the current time tick using {@link Tick Broadcast}.
+ * all other subscribers about the current time tick using {@link TickBroadcast}.
  * This class may not hold references for objects which it is not responsible for.
  * 
  * You can add private fields and public methods to this class.
  * You MAY change constructor signatures and even add new public constructors.
  */
 public class TimeService extends Publisher {
+	Timer timer;
+	int _currentTick;
+	int _maxTicks;
 
-	public TimeService() {
-		super("Change_This_Name");
-		// TODO Implement this
+	public TimeService(int max_ticks) {
+		super("TimerService");
+		_currentTick = 0;
+		_maxTicks = max_ticks;
+		initialize();
 	}
 
 	@Override
 	protected void initialize() {
-		// TODO Implement this
-		
+		timer = new Timer();
 	}
 
 	@Override
 	public void run() {
-		// TODO Implement this
+		while (_currentTick <= _maxTicks) {
+			getSimplePublisher().sendBroadcast( new TickBroadcast(_currentTick));
+			try {
+				Thread.sleep(100);
+			}
+			catch (InterruptedException ignored) {}
+		}
 	}
+
 
 }
