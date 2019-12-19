443e98aea64899ddd3104ed28a25babb13d0ab3b
Idan Tomer <idan.tomer2@gmail.com>
2019-12-19 17:32:24 +0200

    add MI6Runner implementation
    Signed-off-by: Idan Tomer <idan.tomer2@gmail.com>

--- src/main/java/bgu/spl/mics/application/MI6Runner.java
+++ src/main/java/bgu/spl/mics/application/MI6Runner.java
@@ -1,11 +1,131 @@
 package bgu.spl.mics.application;
 
-/** This is the Main class of the application. You should parse the input file,
- * create the different instances of the objects, and run the system.
- * In the end, you should output serialized objects.
+import bgu.spl.mics.application.passiveObjects.*;
+import bgu.spl.mics.application.passiveObjects.MI6RunnerInfo.AgentInfo;
+import bgu.spl.mics.application.passiveObjects.MI6RunnerInfo.MI6RunnerInfo;
+import bgu.spl.mics.application.publishers.TimeService;
+import bgu.spl.mics.application.subscribers.Intelligence;
+import bgu.spl.mics.application.subscribers.M;
+import bgu.spl.mics.application.subscribers.Moneypenny;
+import bgu.spl.mics.application.subscribers.Q;
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * This is the Main class of the application. You should parse the input file, create the different instances of the
+ * objects, and run the system. In the end, you should output serialized objects.
  */
 public class MI6Runner {
+    private static final int ARGS_NUM = 3;
+    private static final int INPUT_FILE = 0;
+    private static final int INVENTORY_FILE = 1;
+    private static final int DIARY_FILE = 2;
+
     public static void main(String[] args) {
-        // TODO Implement this
+        if (args.length != ARGS_NUM) {
+            System.err.println("Invalid num of arguments");
+            return;
+        }
+        FileReader json;
+        try {
+            json = new FileReader(args[INPUT_FILE]);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+        Gson gson = new Gson();
+        MI6RunnerInfo info = gson.fromJson(json, MI6RunnerInfo.class);
+        Inventory.getInstance().load(info.getInventory().toArray(new String[]{}));
+        List<Agent> tempSquad = new ArrayList<>();
+        for (AgentInfo agentInfo : info.getSquad()) {
+            tempSquad.add(new Agent(agentInfo.getSerialNumber(), agentInfo.getName()));
+        }
+        Squad.getInstance().load(tempSquad.toArray(new Agent[]{}));
+        int subscribersNum = info.getServices().getMoneypenny() + info.getServices().getM() +
+                info.getServices().getIntelligence().size() + 1; // + 1 for Q
+        CountDownLatch latch = new CountDownLatch(subscribersNum);
+        ExecutorService e = Executors.newFixedThreadPool(subscribersNum + 1); // + 1 for Time service.
+        e.execute(new Q(latch));
+        for (int i = 0; i < info.getServices().getMoneypenny(); i++) {
+            e.execute(new Moneypenny(i, latch));
+        }
+        for (int i = 0; i < info.getServices().getM(); i++) {
+            e.execute(new M(i, latch));
+        }
+        for (int i = 0; i < info.getServices().getIntelligence().size(); i++) {
+            e.execute(new Intelligence(i, latch, info.getServices().getIntelligence().get(i).getMissions()));
+        }
+        TimeService timeService = new TimeService(info.getServices().getTime(), latch);
+        e.execute(timeService);
+        while (!timeService.isToTerminate()) {
+            try {
+                timeService.wait();
+            } catch (InterruptedException ignored) {
+            }
+        }
+        e.shutdown();
+        try {
+            // time is not really needed because all threads will die gracefully, and so random time was chosen
+            e.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException ignored) {}
+        try {
+            Inventory.getInstance().printToFile(args[INVENTORY_FILE]);
+            Diary.getInstance().printToFile(args[DIARY_FILE]);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
+
+//    /**
+//     * a method that create all necessary M instances
+//     *
+//     * @param numOfMs num of M instances
+//     *
+//     * @return a list of all the created Ms
+//     */
+//    private static List<M> createMs(int numOfMs) {
+//        List<M> Ms = new ArrayList<>();
+//        for (int i = 0; i < numOfMs; i++) {
+//            Ms.add(new M(i));
+//        }
+//        return Ms;
+//    }
+//
+//    /**
+//     * a method that create all necessary Moneypenny instances
+//     *
+//     * @param numOfMoneypennys num of Moneypenny instances
+//     *
+//     * @return a list of all the created Moneypennys
+//     */
+//    private static List<Moneypenny> createMoneypennys(int numOfMoneypennys) {
+//        List<Moneypenny> Moneypennys = new ArrayList<>();
+//        for (int i = 0; i < numOfMoneypennys; i++) {
+//            Moneypennys.add(new Moneypenny(i));
+//        }
+//        return Moneypennys;
+//    }
+//
+//    /**
+//     * a method that create all necessary Intelligence instances
+//     *
+//     * @param intelligences an array of arrays - the outer array represents intelligences, and the inner array holds the
+//     *                      intelligence's missions.
+//     *
+//     * @return a list of all the created Intelligences
+//     */
+//    private static List<Intelligence> createIntelligences(List<List<MissionInfo>> intelligences) {
+//        List<Intelligence> intelligenceObjects = new ArrayList<>();
+//        for (int i = 0; i < intelligences.size(); i++) {
+//            intelligenceObjects.add(new Intelligence(i, intelligences.get(i)));
+//        }
+//        return intelligenceObjects;
+//    }
 }
