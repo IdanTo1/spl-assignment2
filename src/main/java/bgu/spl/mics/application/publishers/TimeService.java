353aa47cbd8d7a25de6a369cd973b4a3ff6cfbd2
Hroy02 <hersh.roy@gmail.com>
2019-12-19 10:04:38 +0200

    Add isToTerminate and formatting

--- src/main/java/bgu/spl/mics/application/publishers/TimeService.java
+++ src/main/java/bgu/spl/mics/application/publishers/TimeService.java
@@ -12,38 +12,42 @@ import java.util.TimerTask;
  * It keeps track of the amount of ticks passed since initialization and notifies
  * all other subscribers about the current time tick using {@link TickBroadcast}.
  * This class may not hold references for objects which it is not responsible for.
- * 
+ * <p>
  * You can add private fields and public methods to this class.
  * You MAY change constructor signatures and even add new public constructors.
  */
 public class TimeService extends Publisher {
-	Timer timer;
-	int _currentTick;
-	int _maxTicks;
+    private Timer timer;
+    private int _currentTick;
+    private final int _maxTicks;
 
-	public TimeService(int max_ticks) {
-		super("TimerService");
-		_currentTick = 0;
-		_maxTicks = max_ticks;
-		initialize();
-	}
+    public TimeService(int max_ticks) {
+        super("TimerService");
+        _currentTick = 0;
+        _maxTicks = max_ticks;
+        initialize();
+    }
 
-	@Override
-	protected void initialize() {
-		timer = new Timer();
-	}
+    @Override
+    protected void initialize() {
+        timer = new Timer();
+    }
 
-	@Override
-	public void run() {
-		while (_currentTick <= _maxTicks) {
-			getSimplePublisher().sendBroadcast( new TickBroadcast(_currentTick));
-			try {
-				Thread.sleep(100);
-			}
-			catch (InterruptedException ignored) {}
-			_currentTick++;
-		}
-	}
+    @Override
+    public void run() {
+        while (_currentTick <= _maxTicks) {
+            getSimplePublisher().sendBroadcast(new TickBroadcast(_currentTick));
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ignored) {
+            }
+            _currentTick++;
+        }
+    }
+
+    public boolean isToTerminate() {
+        return _currentTick > _maxTicks;
+    }
 
 
 }
