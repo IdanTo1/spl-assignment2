9e4ecd273cb399836577d3b6a223ba9f58c648cc
Idan Tomer <idan.tomer2@gmail.com>
2019-12-24 15:30:43 +0200

    add a check for missions order in Intelligence
    Signed-off-by: Idan Tomer <idan.tomer2@gmail.com>

--- src/main/java/bgu/spl/mics/application/subscribers/Intelligence.java
+++ src/main/java/bgu/spl/mics/application/subscribers/Intelligence.java
@@ -7,21 +7,22 @@ import bgu.spl.mics.application.messages.TickBroadcast;
 import bgu.spl.mics.application.passiveObjects.MissionInfo;
 
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.concurrent.CountDownLatch;
 
 /**
- * A Publisher only.
- * Holds a list of Info objects and sends them
+ * A Publisher only. Holds a list of Info objects and sends them
  * <p>
- * You can add private fields and public methods to this class.
- * You MAY change constructor signatures and even add new public constructors.
+ * You can add private fields and public methods to this class. You MAY change constructor signatures and even add new
+ * public constructors.
  */
 public class Intelligence extends Subscriber {
     private int _serial;
     private PriorityQueue<MissionInfo> _missions;
     private CountDownLatch _signalInitialized;
+    private LinkedList<MissionInfo> missionsExecuted;
 
     public Intelligence(int serial, CountDownLatch signalInitialized, List<MissionInfo> missions) {
         super("Intelligence" + serial);
@@ -29,16 +30,31 @@ public class Intelligence extends Subscriber {
         _missions = new PriorityQueue<>(missions.size(), Comparator.comparingInt(MissionInfo::getTimeIssued));
         _missions.addAll(missions);
         _signalInitialized = signalInitialized;
+        missionsExecuted = new LinkedList<>();
     }
 
     @Override
     protected void initialize() {
         subscribeBroadcast(TickBroadcast.class, (TickBroadcast b) -> {
             int currTime = b.getCurrTime();
-            while (_missions.peek() != null && currTime == _missions.peek().getTimeIssued())
-                getSimplePublisher().sendEvent(new MissionReceivedEvent(_missions.poll()));
+            while (_missions.peek() != null && currTime == _missions.peek().getTimeIssued()) {
+                MissionInfo mission = _missions.poll();
+                getSimplePublisher().sendEvent(new MissionReceivedEvent(mission));
+                missionsExecuted.add(mission);
+            }
         });
-        subscribeBroadcast(TerminationTickBroadcast.class, (TerminationTickBroadcast b)->terminate());
+        subscribeBroadcast(TerminationTickBroadcast.class, (TerminationTickBroadcast b) -> {
+            int lastIssued = 0;
+            String lastIssuedName = "";
+            for (MissionInfo mission : missionsExecuted) {
+                if (lastIssued > mission.getTimeIssued())
+                    System.err.println("Error in intelligence" + _serial + ": mission " + mission.getMissionName()
+                            + " executed after " + lastIssuedName);
+                lastIssued = mission.getTimeIssued();
+                lastIssuedName = mission.getMissionName();
+            }
+            terminate();
+        });
         _signalInitialized.countDown();
     }
 
